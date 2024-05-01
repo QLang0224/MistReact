b6ccd2f89e05dc8acdcc3eced7c03900570799d1
QLang0224 <157908068+QLang0224@users.noreply.github.com>
2024-05-01 16:07:30 -0600

    Update and rename movie.js to forecast.js
    Signed-off-by: QLang0224 <157908068+QLang0224@users.noreply.github.com>

--- src/components/forecast.js
+++ src/components/forecast.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch } from 'react-redux'
-import { fetchForecast } from "../actions/movieActions";
+import { fetchForecast } from "../actions/forecastActions";
 
 // support routing
 
